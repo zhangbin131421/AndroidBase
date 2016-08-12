d5a761cbb758cceaaec1b81999b9914414f8b7d4
victor <fengxuanwang@qq.com>
2016-08-12 12:32:47 +0800

    111

--- app/src/main/java/com/carrot/base/androidbase/LoginActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.carrot.base.androidbase;
 
 import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by victor on 8/12/16.
  */
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends Activity {
+
+    @ViewById(R.id.et_login_username)
+    EditText etUsername;
+
+    @ViewById(R.id.et_login_password)
+    EditText etPassword;
+
+    @ViewById(R.id.btn_login_login)
+    Button btnLogin;
+
+    @Click(R.id.btn_login_login)
+    void doLogin(){
+        System.out.println("do login.");
+    }
 
 }
