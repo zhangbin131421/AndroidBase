4879c0465ac428e459eebdef8b5065604f805e51
victor <fengxuanwang@qq.com>
2016-08-12 13:03:10 +0800

    111

--- app/src/main/java/com/carrot/base/androidbase/LoginActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/LoginActivity.java
@@ -4,15 +4,22 @@ import android.app.Activity;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.carrot.base.androidbase.preferences.MyPrefs_;
+
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.sharedpreferences.Pref;
 
 /**
  * Created by victor on 8/12/16.
  */
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends Activity {
+
+
+    @Pref
+    MyPrefs_ myPrefs;
 
     @ViewById(R.id.et_login_username)
     EditText etUsername;
@@ -26,6 +33,14 @@ public class LoginActivity extends Activity {
     @Click(R.id.btn_login_login)
     void doLogin(){
         System.out.println("do login.");
+
+        this.loginSuccess();
+    }
+
+    void loginSuccess(){
+        myPrefs.edit().currentUsername().put(etUsername.getText().toString()).apply();
+
+        this.finish();
     }
 
 }
--- app/src/main/java/com/carrot/base/androidbase/MainActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/MainActivity.java
@@ -4,14 +4,31 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.carrot.base.androidbase.preferences.MyPrefs;
+import com.carrot.base.androidbase.preferences.MyPrefs_;
+
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.sharedpreferences.Pref;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
+    @Pref
+    MyPrefs_ myPrefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LoginActivity_.intent(this).start();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("currentLoginUser: " + myPrefs.currentUsername().get().toString());
+        if(myPrefs.currentUsername().get().equals("")){
+            LoginActivity_.intent(this).start();
+        }
     }
 }
