60d543ef4c08f15e62f7fc3c252d25e557917c2f
victor <fengxuanwang@qq.com>
2016-08-12 10:35:37 +0800

    111

---
+++ app/src/main/java/com/carrot/base/androidbase/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.carrot.base.androidbase;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by victor on 8/12/16.
+ */
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends Activity {
+
+}
--- app/src/main/java/com/carrot/base/androidbase/MainActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/MainActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        LoginActivity_.intent(this).start();
     }
 }
