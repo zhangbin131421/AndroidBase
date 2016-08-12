60d1a73bb4f7b88f30b747ccbcbc819d9d548e2b
victor <fengxuanwang@qq.com>
2016-08-12 14:37:31 +0800

    111

--- app/src/main/java/com/carrot/base/androidbase/MainActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/MainActivity.java
@@ -1,15 +1,21 @@
 package com.carrot.base.androidbase;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.carrot.base.androidbase.preferences.MyPrefs_;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 
 @EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.main)
 public class MainActivity extends AppCompatActivity {
+
 
     @Pref
     MyPrefs_ myPrefs;
@@ -23,11 +29,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        System.out.println("currentLoginUser: " + myPrefs.currentUsername().get().toString());
+
         if(myPrefs.currentUsername().get().equals("")){
             LoginActivity_.intent(MainActivity.this).start();
 
         }
     }
 
+
+    @OptionsItem(R.id.menu_main_setting)
+    void myMethod() {
+        System.out.println("----setting-----");
+    }
 }
