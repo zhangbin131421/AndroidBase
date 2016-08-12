938a54ed7554dd66493cba021492efc5981cc6b2
victor <fengxuanwang@qq.com>
2016-08-12 18:07:33 +0800

    111

--- app/src/main/java/com/carrot/base/androidbase/activity/TaskListActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/activity/TaskListActivity.java
@@ -3,10 +3,14 @@ package com.carrot.base.androidbase.activity;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ListViewCompat;
+import android.widget.ListView;
 import android.widget.Switch;
 
 import com.carrot.base.androidbase.R;
+import com.carrot.base.androidbase.adapter.TaskListAdapter;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
@@ -18,11 +22,18 @@ import org.androidannotations.annotations.ViewById;
 @OptionsMenu(R.menu.task_list)
 public class TaskListActivity extends AppCompatActivity {
 
-//    @ViewById(R.id.swc_task_list_status)
-//    Switch swcStatus;
-//
-//    @ViewById(R.id.lv_task_list_tasks)
-//    ListViewCompat lvTasks;
+    @ViewById(R.id.swc_task_list_status)
+    Switch swcStatus;
 
+    @ViewById(R.id.lv_task_list_tasks)
+    ListView lvTasks;
+
+    @Bean
+    TaskListAdapter taskListAdapter;
+
+    @AfterViews
+    void bindAdapter(){
+        lvTasks.setAdapter(taskListAdapter);
+    }
 
 }
