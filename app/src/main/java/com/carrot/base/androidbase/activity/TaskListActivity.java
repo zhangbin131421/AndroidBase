b5c95e3496982e9032ddae6b1ece3fcda91ac4ec
victor <fengxuanwang@qq.com>
2016-08-13 17:16:14 +0800

    111

--- app/src/main/java/com/carrot/base/androidbase/activity/TaskListActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/activity/TaskListActivity.java
@@ -1,34 +1,22 @@
 package com.carrot.base.androidbase.activity;
 
-import android.content.Context;
-import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import com.carrot.base.androidbase.R;
 import com.carrot.base.androidbase.adapter.TaskListFragmentAdapter;
-import com.carrot.base.androidbase.fragment.TaskListFragment_;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
-import org.androidannotations.annotations.RootContext;
 import org.androidannotations.annotations.ViewById;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by victor on 8/12/16.
  */
 @EActivity(R.layout.activity_task_list)
-//@OptionsMenu(R.menu.task_list)
+@OptionsMenu(R.menu.task_list)
 public class TaskListActivity extends AppCompatActivity {
 
 
@@ -38,10 +26,7 @@ public class TaskListActivity extends AppCompatActivity {
     @ViewById(R.id.vp_task_list_pager)
     ViewPager vpPager;
 
-//    @RootContext
-//    Context context;
-//
-//    @Bean
+
     TaskListFragmentAdapter taskListFragmentAdapter;
 
     @AfterViews
@@ -50,7 +35,7 @@ public class TaskListActivity extends AppCompatActivity {
         vpPager.setAdapter(taskListFragmentAdapter);
 
         tlTabs.setupWithViewPager(vpPager);
-//        tlTabs.
+
     }
 
 }
