3a00102d9165b20ee18d1bd81812cc0499b799c8
victor <fengxuanwang@qq.com>
2016-08-13 16:22:56 +0800

    111

--- app/src/main/java/com/carrot/base/androidbase/activity/TaskListActivity.java
+++ app/src/main/java/com/carrot/base/androidbase/activity/TaskListActivity.java
@@ -1,17 +1,28 @@
 package com.carrot.base.androidbase.activity;
 
-import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.carrot.base.androidbase.R;
 import com.carrot.base.androidbase.adapter.TaskListFragmentAdapter;
+import com.carrot.base.androidbase.fragment.TaskListFragment_;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.RootContext;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by victor on 8/12/16.
@@ -20,19 +31,24 @@ import org.androidannotations.annotations.ViewById;
 @OptionsMenu(R.menu.task_list)
 public class TaskListActivity extends AppCompatActivity {
 
-//    @ViewById(R.id.tl_task_list_sliding_tabs)
-//    TabLayout tlTabs;
 
-//    @ViewById(R.id.vp_task_list_viewpager)
-//    ViewPager vpViewPage;
+    @ViewById(R.id.tl_task_list_tabs)
+    TabLayout tlTabs;
+
+    @ViewById(R.id.vp_task_list_pager)
+    ViewPager vpPager;
 
 //    @RootContext
 //    Context context;
+//
+//    @Bean
+    TaskListFragmentAdapter taskListFragmentAdapter;
 
-//    @AfterViews
-//    void initAdapter(){
-//        vpViewPage.setAdapter(new TaskListFragmentAdapter(getSupportFragmentManager(), TaskListActivity.this));
-//        tlTabs.setupWithViewPager(vpViewPage);
-//    }
+    @AfterViews
+    void initTabs(){
+        taskListFragmentAdapter = new TaskListFragmentAdapter(getSupportFragmentManager(), getApplicationContext());
+        vpPager.setAdapter(taskListFragmentAdapter);
+//        tlTabs.
+    }
 
 }
